111d9d88713db685091e407b8c9d031e6a8ddf5d
Evan Lovett <evanjameslovett@gmail.com>
2026-08-22 13:30:53 -0400

    Update HabitList.tsx

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,13 +1,15 @@
 export function HabitList() {
-  const habits = []
+  const habits = ["this is a new habit", "and another"]
 
   if (habits.length === 0) {
-    return <p className="text-center">No habits yet.</p>
+    return <p className="text-center text-zinc-500! py-12">No habits yet.</p>
   }
 
   return (
-    <div>
-
+    <div className="flex flex-col gap-2">
+      {habits.map((habit, index) => (
+        <p key={index}>{habit}</p>
+      ))}
     </div>
   )
 }
