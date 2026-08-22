0f48d9b9b868f2bc612e090f891e261910c77467
Evan Lovett <evanjameslovett@gmail.com>
2026-08-22 13:25:08 -0400

    add habit list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header"
 import { HabitForm } from "./components/HabitForm"
-
+import { HabitList } from "./components/HabitList"
 
 export default function App() {
   return (
     <div className="max-w-2xl mx-auto p-4 flex flex-col">
       <Header />
       <HabitForm />
+      <HabitList />
     </div>
   )
 }
