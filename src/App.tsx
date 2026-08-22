c45d38896b1387a4586acf42164a817fb4fc3f61
Evan Lovett <evanjameslovett@gmail.com>
2026-08-22 13:03:22 -0400

    set up baseline header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 export default function App() {
   return (
-    <h1 className="text-red-500">Test</h1>
+    <div className="max-w-2xl mx-auto p-4 flex flex-col">
+      <Header />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-3xl font-bold">Simplified Habit Tracker</h1>
+        <span className="text-zinc-400 text-sm">1/1 done today</span>
+      </div>
+      <div className="flex flex-col gap-1">
+        <span>Apr 6 - Apr 12</span>
+        <div className="flex items-baseline gap-1">
+          <button>Prev</button>
+          <button>Next</button>
+        </div>
+      </div>
+    </header>
   )
 }
