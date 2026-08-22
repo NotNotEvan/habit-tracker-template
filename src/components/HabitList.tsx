feced052a2d87dfb6fb3d6ae2041709d2b670d56
Evan Lovett <evanjameslovett@gmail.com>
2026-08-22 13:25:14 -0400

    Create HabitList.tsx

---
+++ src/components/HabitList.tsx
@@ -0,0 +1,13 @@
+export function HabitList() {
+  const habits = []
+
+  if (habits.length === 0) {
+    return <p className="text-center">No habits yet.</p>
+  }
+
+  return (
+    <div>
+
+    </div>
+  )
+}
